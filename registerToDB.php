9c5619d84d89ea96efb3d62918e4dbacc7ec20bc
Thomas <38799842+RadiantBytes@users.noreply.github.com>
2019-04-30 16:18:50 -0500

    Add files via upload

--- registerToDB.php
+++ registerToDB.php
@@ -8,39 +8,42 @@ $password = filter_input(INPUT_POST, 'reg_password');
 //	Encrypt password
 $password = md5($password);
 
-	if (!empty($password)){
-			$host = "localhost";
-			$dbusername = "root";
-			$dbpassword = "OmeletPenguin0!";
-			$dbname = "shoppingcartdb";
-			
-			// Connect to ShoppingCartDB
-			$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-			
-			if (mysqli_connect_error()) {
-				die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-			}
-			else {
-				$sql = "INSERT INTO register_user (LastName, FirstName, password, email, phoneNumber)
-				values ('$LastName','$FirstName','$password','$email','$phoneNumber')";
-				
-				if ($conn->query($sql)){
-			echo"Congratulations "; echo '<br />';
-			echo"Your Registration was successful "; echo '<br />';
-			echo "go back to sign in "; echo '<br />';
-			}
-			else{
+if (!empty($password)){
+	$host = "localhost";
+	$dbusername = "root";
+	$dbpassword = "";
+	$dbname = "shoppingcartdb";
+
+	// Connect to ShoppingCartDB
+	$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+	if (mysqli_connect_error()) {
+		die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	}
+	else {
+		$sql = "INSERT INTO register_user (LastName, FirstName, password, email, phoneNumber)
+			values ('$LastName','$FirstName','$password','$email','$phoneNumber')";
+
+		if ($conn->query($sql)){
+
+
+			$data ='{username:"; . username . '",password:"; . $password .'"}';
+			echo json_encode($data);
+
+			header('Location: VELZ.html');	
+		}
+		else{
 			echo "Error: ". $sql ."
-			". $conn->error;
-			}
-			
-			$conn->close();
-	
-			
+				". $conn->error;
 		}
-		}
-		else {
-			echo "All fields are required";
-			die();
-		}
+
+		$conn->close();
+
+
+	}
+}
+else {
+	echo "All fields are required";
+	die();
+}
 ?>
